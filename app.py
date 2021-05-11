efd06e25268efae1eb3bfe09ab7c184e2c7ff1bf
Tri <trideep.india@gmail.com>
2021-05-11 18:37:02 +0530

    Webpage with Flask api added

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+def homeform():
+    if request.method == 'POST':
+      fname = request.form['fname']
+      cough = request.form['cough']
+      fever = request.form['fever']
+      sore_throat = request.form['sore_throat']
+      shortness_of_breath = request.form['shortness_of_breath']
+      headache = request.form['headache']
+      test_indication = request.form['test_indication']
+      
+      result = "Positive"
+
+      return render_template('index.html',result=result)
+    
+if __name__ == '__main__':
+    app.run(debug = True)
