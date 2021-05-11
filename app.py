9402d8702a1402f132d64bf43c6a137d7168d982
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2021-05-11 19:33:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def homeform():
       return render_template('index.html',result=result,name=fname)
     
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = False,host='0.0.0.0')
