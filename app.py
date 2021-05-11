156d2bbdb32edcb507417887a5ebecebfd6c35eb
Tri <trideep.india@gmail.com>
2021-05-11 19:14:59 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import numpy as np
+import pandas as pd
 from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open('Eyantra.pickle', 'rb'))
 
 @app.route('/')
 def home():
@@ -14,10 +18,15 @@ def homeform():
       fever = request.form['fever']
       sore_throat = request.form['sore_throat']
       shortness_of_breath = request.form['shortness_of_breath']
-      headache = request.form['headache']
+      head_ache = request.form['headache']
       test_indication = request.form['test_indication']
       print(fname,cough,fever,sore_throat,shortness_of_breath,headache,test_indication)
-      result = "Positive"
+      prediction = model.predict([[cough,fever,sore_throat,shortness_of_breath,head_ache,test_indication]])
+      result=""
+      if prediction.all() == 0:
+        result="Negative"
+      elif prediction.all() == 1:
+        result="Positive"
 
       return render_template('index.html',result=result,name=fname)
     
